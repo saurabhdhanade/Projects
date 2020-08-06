12092aff299fe340c821a950a8856611a9123aa9
saurabhdhanade <62280228+saurabhdhanade@users.noreply.github.com>
2020-08-06 19:52:34 +0530

    Update star_patterns.py

--- star_patterns.py
+++ star_patterns.py
@@ -1,3 +1,4 @@
+#pyramid pattern
 def pattern(n):
     k = 2 * n - 2
     for i in range(0, n):
@@ -10,3 +11,74 @@ def pattern(n):
 
 
 pattern(5)
+
+#reverse pyramid
+def reversepyra(n):
+    k = n * 2 - 2
+    for i in range (n,-1,-1):
+        for j in range (k,0,-1):
+            print(end=" ")
+        k=k+1
+        for j in range (0,i+1):
+            print("*",end=" ")
+        print()
+
+reversepyra(5)
+
+#right side tringle pattern
+def pattern(n):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print("*", end=' ')
+        print()
+    for i in range(n, 0, -1):
+        for j in range(0, i + 1):
+            print("*", end=' ')
+        print()
+
+
+pattern(5)
+
+#hourglass pattern
+def pattern(n):
+    k = n - 2
+    for i in range(n, -1, -1):
+        for j in range(k, 0, -1):
+            print(end=" ")
+        k = k + 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print()
+    k = 2 * n - 2
+    for i in range(0, n + 1):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print()
+        
+pattern(5)
+
+#right angle tringle pattern
+def rightangle(n):
+    for i in range(0,n):
+        for j in range(0,1+i):
+            print("*", end=" ")
+        print()
+rightangle(5)
+
+#right side right angle tringle
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0, i + 1):
+            print("*", end=" ")
+        print()
+
+
+pattern(5)
+
