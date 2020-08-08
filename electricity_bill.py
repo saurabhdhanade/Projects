7a416e61829f71e473b9ad35200016aabaa4aafa
saurabhdhanade <62280228+saurabhdhanade@users.noreply.github.com>
2020-08-08 09:29:59 +0530

    Update electricity_bill.py

--- electricity_bill.py
+++ electricity_bill.py
@@ -1,10 +1,14 @@
 def electricBill(units):
+  
   if units<=100:
       return 100*10
+  
   elif units<=200:
       return (100*10) + (units-100)*15
+  
   elif units<=300:
       return (100*10) + (100*15) + (units-200)*20
+  
   elif units<=400:
       return (100*10)+(100*15)+(100*20)+(units-300)*25
 
